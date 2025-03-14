585e87e2759389bb2cdd6cdc3b18628a4d3af54d
Niko Nikolov <nikolay.niko.nikolov@gmail.com>
2025-03-14 14:37:53 -0700

    combined primary_app_dev with prototype

--- src/main.h
+++ src/main.h
@@ -21,12 +21,12 @@
 #include "timers.h"
 
 /* Project-specific includes */
+#include "bme280.h"
+#include "lcd.h"
 #include "nexys4IO.h"
 #include "pidtask.h"
 #include "platform.h"
 #include "tsl2561.h"
-#include "bme280.h"
-#include "lcd.h"
 
 /* Definitions for NEXYS4IO Peripheral */
 #define N4IO_DEVICE_ID XPAR_NEXYS4IO_0_DEVICE_ID
@@ -40,37 +40,46 @@
 #define BTN_CHANNEL 1
 #define SW_CHANNEL 2
 
-#define mainQUEUE_LENGTH (1)
+#define mainQUEUE_LENGTH ( 1 )
 #define mainDONT_BLOCK (portTickType) 0
 
 #define max_duty 255
 #define min_duty 0
 #define lux_mask 0xFFFF
 
-#define UPDATE_SATURATING(val, inc, min_val, max_val, increase) \
-    do { \
-        if (increase) { \
-            if ((val + inc) < max_val) val += inc; \
-            else val = max_val; \
-        } else { \
-            if (val > inc) val -= inc; \
-            else val = min_val; \
-        } \
-    } while (0)
+#define UPDATE_SATURATING( val, inc, min_val, max_val, increase ) \
+    do                                                            \
+    {                                                             \
+        if ( increase )                                           \
+        {                                                         \
+            if ( ( val + inc ) < max_val )                        \
+                val += inc;                                       \
+            else                                                  \
+                val = max_val;                                    \
+        }                                                         \
+        else                                                      \
+        {                                                         \
+            if ( val > inc )                                      \
+                val -= inc;                                       \
+            else                                                  \
+                val = min_val;                                    \
+        }                                                         \
+    } while ( 0 )
 
 /* Global Instances */
-extern XGpio             xInputGPIOInstance;
-extern XIic              IicInstance;
+extern XGpio                    xInputGPIOInstance;
+extern XIic                     IicInstance;
 extern struct bme280_calib_data calib_data;
-extern SemaphoreHandle_t binary_sem;
-extern SemaphoreHandle_t bme280_sem;
-extern SemaphoreHandle_t oled_sem;
-extern SemaphoreHandle_t i2c_sem;  // Global I2C semaphore
-extern xQueueHandle      toPID;
-extern xQueueHandle      fromPID;
+extern SemaphoreHandle_t        binary_sem;
+extern SemaphoreHandle_t        bme280_sem;
+extern SemaphoreHandle_t        oled_sem;
+extern SemaphoreHandle_t        i2c_sem;  // Global I2C semaphore
+extern xQueueHandle             toPID;
+extern xQueueHandle             fromPID;
 
 /* Structure to hold sensor data for LCD */
-typedef struct {
+typedef struct
+{
     int32_t  temperature; /* In hundredths of °C */
     uint32_t pressure;    /* In Pa */
     uint32_t humidity;    /* In 1024ths of % */
@@ -80,14 +89,14 @@ typedef struct {
 extern SensorData_t sensor_data;
 
 /* Function Declarations */
-void prvSetupHardware(void);  // Remove static, defined in main.c
-void gpio_intr(void* pvUnused);  // Remove static, defined in main.c
-int  do_init(void);
-void Parse_Input_Task(void* p);
-void PID_Task(void* p);
-void Display_Task(void* p);
-bool pid_init(PID_t* pid);
-float pid_funct(PID_t* pid, uint16_t lux_value, uint8_t switches);
-void print_pid(PID_t* pid);
+void  prvSetupHardware ( void );     // Remove static, defined in main.c
+void  gpio_intr ( void* pvUnused );  // Remove static, defined in main.c
+int   do_init ( void );
+void  Parse_Input_Task ( void* p );
+void  PID_Task ( void* p );
+void  Display_Task ( void* p );
+bool  pid_init ( PID_t* pid );
+float pid_funct ( PID_t* pid, uint16_t lux_value, uint8_t switches );
+void  print_pid ( PID_t* pid );
 
 #endif /* MAIN_H */
